c6862af42fb9c90bd61c907ba9dbceac9497a139
zhanybekk0v <zhanybekovnurbol@hmail.com>
2023-01-24 19:21:59 +0600

    navbar, routes, pages, components

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Avatar, Grid, MenuItem } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Logo from '../img/bookLogo.png'
+
+const pages = [
+  { name: 'HOME', link: '/', id: 1 },
+  { name: 'PRODUCTS', link: '/products', id: 3 },
+  { name: 'ABOUT US', link: '/about', id: 4 },
+  { name: 'ADMIN', link: '/admin', id: 2 },
+
+];
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }} >
+      <AppBar position="static" color='warning' >
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }} >
+          <Avatar alt="Remy Sharp" src={Logo} sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            {pages.map((page) => (
+              <MenuItem  key={page.id} >
+                <Link to={page.link}>
+                  <Typography  color="white" textAlign="end">{page.name}</Typography>
+                </Link>
+              </MenuItem>
+            ))}
+          </Box>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ src/routes/MainRoutes.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import AboutUsPage from '../pages/AboutUsPage'
+import AdminPage from '../pages/AdminPage'
+import AuthPage from '../pages/AuthPage'
+import CartPage from '../pages/CartPage'
+import EditProductPage from '../pages/EditProductPage'
+import HomePage from '../pages/HomePage'
+import NotFoundPage from '../pages/NotFoundPage'
+import ProductsPage from '../pages/ProductsPage'
+
+const MainRoutes = () => {
+  const PUBLIC_ROUTES = [
+    {
+      link: '/',
+      element: <HomePage />,
+      id: 1
+    },
+    {
+      link: '/products',
+      element: <ProductsPage />,
+      id: 2
+    },
+    {
+      link: '/cart',
+      element: <CartPage />,
+      id: 3
+    },
+    {
+      link: '/edit/:id',
+      element: <EditProductPage />,
+      id: 4
+    },
+    {
+      link: '/admin',
+      element: <AdminPage />,
+      id: 5
+    },
+    {
+      link: '*',
+      element: <NotFoundPage />,
+      id: 6
+    },
+    {
+      link: '/auth',
+      element: <AuthPage />,
+      id: 7
+    },
+    {
+      link: '/about',
+      element: <AboutUsPage />,
+      id: 8
+    }
+  ]
+  return (
+    <>
+    <Routes>
+    {PUBLIC_ROUTES.map((item) => (<Route path={item.link} element={item.element} key={item.id} />))}
+    </Routes>
+
+    </>
+  )
+}
+
+export default MainRoutes
