945d36f5aeb54cd5eecace001530d0f707d7c3af
unknown <a1ishkaajibek@gmail.com>
2023-01-24 20:26:34 +0600

    Admin panel

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import { Box, Button, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { useProducts } from '../../contexts/ProductContextProvider';
 
 const AddProduct = () => {
-  return (
-    <div>AddProduct</div>
-  )
-}
+  const [product, setProduct] = useState({
+    name: '',
+    descr: '',
+    price: 0,
+    picture: '',
+    type: '',
+  });
 
-export default AddProduct
+  // const { addProduct } = useProducts();
+
+  const handleInp = (e) => {
+    if (e.target.name === 'price') {
+      let obj = {
+        ...product,
+        [e.target.name]: Number(e.target.value),
+      };
+      setProduct(obj);
+    } else {
+      let obj = {
+        ...product,
+        [e.target.name]: e.target.value,
+      };
+      setProduct(obj);
+    }
+  };
+
+
+  return (
+    <Box sx={{ width: '60vw', margin: '10vh auto' }}>
+      <Typography align='center' variant='h4' element='h2'>
+        ADMIN PAGE
+      </Typography>
+      <TextField
+        onChange={handleInp}
+        fullWidth
+        name='picture'
+        id='outlined-basic'
+        label='picture'
+        variant='outlined'
+      />
+      <TextField
+        onChange={handleInp}
+        fullWidth
+        name='name'
+        id='outlined-basic'
+        label='name'
+        variant='outlined'
+      />
+      <TextField
+        onChange={handleInp}
+        fullWidth
+        name='descr'
+        id='outlined-basic'
+        label='description'
+        variant='outlined'
+      />
+      <TextField
+        onChange={handleInp}
+        fullWidth
+        name='descr'
+        id='outlined-basic'
+        label='Author'
+        variant='outlined'
+      />
+      <TextField
+        onChange={handleInp}
+        fullWidth
+        name='price'
+        id='outlined-basic'
+        label='price'
+        variant='outlined'
+      />
+      <TextField
+        onChange={handleInp}
+        fullWidth
+        name='type'
+        id='outlined-basic'
+        label='type'
+        variant='outlined'
+      />
+      <Button
+        // onClick={() => addProduct(product)}
+        variant='outlined'
+        fullWidth
+        size='large'
+      >
+        CREATE PRODUCT
+      </Button>
+    </Box>
+  );
+};
+
+export default AddProduct;
