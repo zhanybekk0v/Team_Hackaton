d16fb985bf7fb6a35bcb805dc24d983e7b90be25
zhanybekk0v <zhanybekovnurbol@hmail.com>
2023-01-24 20:07:35 +0600

    context

---
+++ src/contexts/ProductContextProvider.js
@@ -0,0 +1,44 @@
+import { AccessTimeTwoTone } from '@mui/icons-material'
+import React, { createContext, useContext, useReducer } from 'react'
+import { ACTIONS } from '../helper/consts'
+
+export const productContext = createContext()
+
+export const useProduct = () => {
+  return useContext(productContext)
+}
+
+const INIT_STATE = {
+  products: [],
+  productsDetails: {}
+}
+const reducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case ACTIONS.GET_PRODUCTS:
+      return { ...state, products: action.payload }
+    case ACTIONS.GET_PRODUCTS_DETAILS:
+      return { ...state, productsDetails: action.payload }
+
+    default:
+      return state
+  }
+}
+
+const ProductContextProvider = ({ children }) => {
+const [state, dispatch] = useReducer(reducer, INIT_STATE)
+
+//? GET запрос
+async function getProducts() {
+  const {data} = await axios
+}
+  const values = {
+
+  }
+  return (
+    <productContext.Provider value={values}>
+      {children}
+    </productContext.Provider>
+  )
+}
+
+export default ProductContextProvider
---
+++ src/helper/consts.js
@@ -0,0 +1,7 @@
+export const API = ' http://localhost:8000/products'
+
+
+export const ACTIONS = {
+  GET_PRODUCTS: 'GET_PRODUCTS',
+  GET_PRODUCTS_DETAILS: 'GET_PRODUCTS_DETAILS'
+}
