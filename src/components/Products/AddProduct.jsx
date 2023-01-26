6bc44d0944e2856720bebe6534d8633597d77fd0
zhanybekk0v <zhanybekovnurbol@hmail.com>
2023-01-26 17:24:24 +0600

    Дизайн

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -1,6 +1,7 @@
 import { Box, Button, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import { useProduct } from '../../contexts/ProductContextProvider';
+import booksFon from '../../img/booksFon.jpg'
 
 const AddProduct = () => {
 
@@ -34,62 +35,68 @@ const AddProduct = () => {
 
 
   return (
-    <Box sx={{ width: '60vw', margin: '10vh auto' }}>
-      <Typography align='center' variant='h4' element='h2'>
+    <Box sx={{ width: '60vw', padding: '60px', textAlign: 'center'}}>
+      <Typography sx={{marginBottom: 3}} align='center' variant='h4' element='h2'>
         ADMIN PAGE
       </Typography>
       <TextField
+        sx={{marginBottom: 2, backgroundColor: '#f6e4de'}}
         onChange={handleInp}
         fullWidth
         name='picture'
         id='outlined-basic'
-        label='picture'
+        label='Picture'
         variant='outlined'
       />
       <TextField
+        sx={{marginBottom: 2, backgroundColor: '#f6e4de'}}
         onChange={handleInp}
         fullWidth
         name='name'
         id='outlined-basic'
-        label='name'
+        label='Name'
         variant='outlined'
       />
       <TextField
+        sx={{marginBottom: 2, backgroundColor: '#f6e4de'}}
         onChange={handleInp}
         fullWidth
         name='descr'
         id='outlined-basic'
-        label='description'
+        label='Description'
         variant='outlined'
       />
       <TextField
+        sx={{marginBottom: 2, backgroundColor: '#f6e4de'}}
         onChange={handleInp}
         fullWidth
-        name='descr'
+        name='author'
         id='outlined-basic'
         label='Author'
         variant='outlined'
       />
       <TextField
+        sx={{marginBottom: 2, backgroundColor: '#f6e4de'}}
         onChange={handleInp}
         fullWidth
         name='price'
         id='outlined-basic'
-        label='price'
+        label='Price'
         variant='outlined'
       />
       <TextField
+        sx={{marginBottom: 2,  backgroundColor: '#f6e4de'}}
         onChange={handleInp}
         fullWidth
         name='type'
         id='outlined-basic'
-        label='type'
+        label='Type'
         variant='outlined'
       />
       <Button
+        sx={{ width: 200 , color: 'brown', border: '2px solid brown',backgroundColor: '#f6e4de'}}
         onClick={() => addProduct(product)}
         variant='outlined'
-        fullWidth
         size='large'
       >
         CREATE PRODUCT
